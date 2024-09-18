e798ac640cc99b51ee52c79b3056efbc8e3c6837
MatteoPalazzo <palazzo.matteo@educ.di.unito.it>
2024-09-18 15:32:55 +0200

    Old 'pop' and 'push' functions defined with 4 cases, are respectively renamed to 'po' and 'pu'

--- SCORE/push_pop_definitions.v
+++ SCORE/push_pop_definitions.v
@@ -3,8 +3,6 @@ From Coq Require Import ZArith.
 From Coq Require Import List.
 Import ListNotations. 
 
-From CDF Require Import common_parts.
-
 Local Open Scope string_scope.
 Local Open Scope Z_scope.
 Local Open Scope list_scope.
@@ -12,7 +10,7 @@ Local Open Scope list_scope.
 (*  ******************************** *)
 (** * PUSH and POP: Matteo's version *)
 
-Definition pop (p : Z * list Z * nat) : Z * list Z * nat := 
+Definition po (p : Z * list Z * nat) : Z * list Z * nat := 
 match p with 
 | (v,   [],   n) => (v,   [], S n)
 | (0, v::t,   O) => (v,    t,   O)
@@ -27,7 +25,7 @@ end.
 - [pop (k,v::t,n) = (k, v::t, S n)], like in the previous case, it is not possible to eliminate the top of the stack when there are previous wrong attemnpts to forget the top. Moreover, since the current value of the variable is not 0 this is a further wrong attempt to discard the top of the stack. The counter keeps track of it.
 *)
 
-Definition push (p : Z * list Z * nat) : (Z * list Z * nat) := 
+Definition pu (p : Z * list Z * nat) : (Z * list Z * nat) := 
 match p with 
 | (v,   [], S n) => (v,   [],   n)
 | (v,    t,   O) => (0, v::t,   O) 
@@ -38,8 +36,8 @@ end.
 (** The definition of [push], "complements" the one of [pop], the "main"case being [pop (v,t,O) = (0,v::t,O)] which stores the current value of the variable on top of the stack, resetting it to 0, because no there is no trace of some pending wrong occurrence of [pop].
 *)
 
-Example pop00: pop (1, [], O) = (1, [], 1%nat).
+Example po00: po (1, [], O) = (1, [], 1%nat).
 Proof. auto. Qed.
-Example pop01: pop (2, [1], O) = (2, [1], 1%nat).
+Example po01: po (2, [1], O) = (2, [1], 1%nat).
 Proof. auto. Qed.
 
--- SCORE/push_pop_properties.v
+++ SCORE/push_pop_properties.v
@@ -3,7 +3,6 @@ From Coq Require Import ZArith.
 From Coq Require Import List.
 Import ListNotations.
 
-From CDF Require Import common_parts.
 From CDF Require Import push_pop_definitions.
 
 Local Open Scope string_scope.
@@ -13,21 +12,21 @@ Local Open Scope list_scope.
 (*  ******************************************** *)
 (** * Proof of "push/pop are each other inverse" *)
 
-Lemma push_inv_pop : forall (x: Z * list Z * nat), 
-  push (pop x) = x.
+Lemma pu_inv_po : forall (x: Z * list Z * nat), 
+  pu (po x) = x.
 Proof. intro p. destruct p as ((v, s), c).
 destruct v as [ | vp | vn ].
 - (* v = 0 *)
   destruct c as [ | c' ].
   + destruct s as [ | h t ].
     * reflexivity. 
-    * unfold pop; 
+    * unfold po; 
       destruct h as [ | hp | hn ];
       destruct t as [ | h' t']; 
       reflexivity.
   + destruct s as [ | h t ].
     * reflexivity. 
-    * unfold pop; 
+    * unfold po; 
       destruct h as [ | hp | hn ];
       destruct t as [ | h' t']; 
       reflexivity.
@@ -35,13 +34,13 @@ destruct v as [ | vp | vn ].
   destruct c as [ | c' ].
   + destruct s as [ | h t ].
     * reflexivity. 
-    * unfold pop; 
+    * unfold po; 
       destruct h as [ | hp | hn ];
       destruct t as [ | h' t']; 
       reflexivity.
   + destruct s as [ | h t ].
     * reflexivity. 
-    * unfold pop; 
+    * unfold po; 
       destruct h as [ | hp | hn ];
       destruct t as [ | h' t']; 
       reflexivity.
@@ -49,34 +48,34 @@ destruct v as [ | vp | vn ].
   destruct c as [ | c' ].
   + destruct s as [ | h t ].
     * simpl. reflexivity. 
-    * unfold pop; 
+    * unfold po; 
       destruct h as [ | hp | hn ];
       destruct t as [ | h' t']; 
       reflexivity.
   + destruct s as [ | h t ].
     * simpl. reflexivity. 
-    * unfold pop; 
+    * unfold po; 
       destruct h as [ | hp | hn ];
       destruct t as [ | h' t']; 
       reflexivity.
 Qed.
 
 
-Lemma pop_inv_push : forall (x: Z * list Z * nat), 
-  pop (push x) = x.
+Lemma po_inv_pu : forall (x: Z * list Z * nat), 
+  po (pu x) = x.
 Proof. intro p. destruct p as ((v, s), c).
 destruct v as [ | vp | vn ].
 - (* v = 0 *)
   destruct c as [ | c' ].
   + destruct s as [ | h t ].
     * reflexivity. 
-    * unfold pop; 
+    * unfold po; 
       destruct h as [ | hp | hn ];
       destruct t as [ | h' t']; 
       reflexivity.
   + destruct s as [ | h t ].
     * reflexivity. 
-    * unfold pop; 
+    * unfold po; 
       destruct h as [ | hp | hn ];
       destruct t as [ | h' t']; 
       reflexivity.
@@ -84,13 +83,13 @@ destruct v as [ | vp | vn ].
   destruct c as [ | c' ].
   + destruct s as [ | h t ].
     * reflexivity. 
-    * unfold pop; 
+    * unfold po; 
       destruct h as [ | hp | hn ];
       destruct t as [ | h' t']; 
       reflexivity.
   + destruct s as [ | h t ].
     * reflexivity. 
-    * unfold pop; 
+    * unfold po; 
       destruct h as [ | hp | hn ];
       destruct t as [ | h' t']; 
       reflexivity.
@@ -98,22 +97,22 @@ destruct v as [ | vp | vn ].
   destruct c as [ | c' ].
   + destruct s as [ | h t ].
     * simpl. reflexivity. 
-    * unfold pop; 
+    * unfold po; 
       destruct h as [ | hp | hn ];
       destruct t as [ | h' t']; 
       reflexivity.
   + destruct s as [ | h t ].
     * simpl. reflexivity. 
-    * unfold pop; 
+    * unfold po; 
       destruct h as [ | hp | hn ];
       destruct t as [ | h' t']; 
       reflexivity.
 Qed.
 
-Theorem pop_invertible : forall p : Z * list Z * nat, 
-  (pop (push p) = p) /\ (push (pop p) = p).
+Theorem po_inv_pu_inv_po : forall p : Z * list Z * nat, 
+  (po (pu p) = p) /\ (pu (po p) = p).
 Proof. destruct p as ((v, s), c). intros. 
 split.
-- apply pop_inv_push.
-- apply push_inv_pop.
+- apply po_inv_pu.
+- apply pu_inv_po.
 Qed.
