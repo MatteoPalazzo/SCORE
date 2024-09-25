3d78bd6f79d1f35bd442958b084f37f6fb7ac0dd
MatteoPalazzo <palazzo.matteo@educ.di.unito.it>
2024-09-25 13:39:26 +0200

    Added some more comments on the functions.

--- SCORE/push_pop_definitions.v
+++ SCORE/push_pop_definitions.v
@@ -8,41 +8,20 @@ Local Open Scope Z_scope.
 Local Open Scope list_scope.
 
 (*  ******************************** *)
-(** * PUSH and POP: 4 cases version *)
-
-Definition po (p : Z * list Z * nat) : Z * list Z * nat := 
-match p with 
-| (v,   [],   n) => (v,   [], S n)
-| (0, v::t,   O) => (v,    t,   O)
-| (0, v::t, S n) => (0, v::t, S n)
-| (k, v::t,   n) => (k, v::t, S n)
-end.
-
-(** Intution about the definition of [pop]:
-- [po (v,[],n) = (v,[],S n)] manages the first attempt to eliminate the top in an empty list. That is not possible and the counter counts that attempt;
-- [po (0,v::t,O) = (v,t,0)] correctly eliminates the top of a stack because: (i) there is no trace of wrong attempts in the counter, (ii) the current value being overwritten is 0;
-- [po (0,v::t,S n) = (0, v::t, S n)] does not allow to eliminate the top of the stack because there are previous wrong attemnpts to forget the top, even if the current value of the variable is 0 and it could be  forgotten;
-- [po (k,v::t,n) = (k, v::t, S n)], like in the previous case, it is not possible to eliminate the top of the stack when there are previous wrong attemnpts to forget the top. Moreover, since the current value of the variable is not 0 this is a further wrong attempt to discard the top of the stack. The counter keeps track of it.
-*)
-
-Definition pu (p : Z * list Z * nat) : (Z * list Z * nat) := 
-match p with 
-| (v,   [], S n) => (v,   [],   n)
-| (v,    t,   O) => (0, v::t,   O) 
-| (0, v::t, S n) => (0, v::t, S n)
-| (k, v::t, S n) => (k, v::t,   n) 
-end.
-
-(** The definition of [pu], "complements" the one of [po], the "main"case being [po (v,t,O) = (0,v::t,O)] which stores the current value of the variable on top of the stack, resetting it to 0, because no there is no trace of some pending wrong occurrence of [po].
-*)
-
-Example po00: po (1, [], O) = (1, [], 1%nat).
-Proof. auto. Qed.
-Example po01: po (2, [1], O) = (2, [1], 1%nat).
-Proof. auto. Qed.
+(** * PUSH and POP: two versions     *)
+(** We hereby presents two versions  
+    of the same functions PUSH and
+    POP. The first pair (pop, push)
+    is the one presented in the paper 
+    "Reversible Computation with 
+    Stacks and 'Reversible Management 
+    of Failures'"; the second one 
+    (pop4, push4)
+    is an extended more detailed
+    version.                         *)
 
 (*  ******************************** *)
-(** * PUSH and POP: 3 cases version *)
+(** ** PUSH and POP: 3 cases version *)
 
 Definition pop (st : Z * (list Z) * nat) : Z * (list Z) * nat := 
 match st with 
@@ -57,3 +36,33 @@ match st with
 | (0, v::t, S c) => (0, v::t, S c)
 | (v,    s, S c) => (v,    s,   c)
 end.
+
+(*  ******************************** *)
+(** ** PUSH and POP: 4 cases version *)
+
+Definition pop4 (p : Z * list Z * nat) : Z * list Z * nat := 
+match p with 
+| (v,   [],   n) => (v,   [], S n)
+| (0, v::t,   O) => (v,    t,   O)
+| (0, v::t, S n) => (0, v::t, S n)
+| (k, v::t,   n) => (k, v::t, S n)
+end.
+
+(** Intution about the definition of [pop4]:
+- [pop4 (v,[],n) = (v,[],S n)] manages the first attempt to eliminate the top in an empty list. That is not possible and the counter counts that attempt;
+- [pop4 (0,v::t,O) = (v,t,0)] correctly eliminates the top of a stack because: (i) there is no trace of wrong attempts in the counter, (ii) the current value being overwritten is 0;
+- [pop4 (0,v::t,S n) = (0, v::t, S n)] does not allow to eliminate the top of the stack because there are previous wrong attemnpts to forget the top, even if the current value of the variable is 0 and it could be  forgotten;
+- [pop4 (k,v::t,n) = (k, v::t, S n)], like in the previous case, it is not possible to eliminate the top of the stack when there are previous wrong attemnpts to forget the top. Moreover, since the current value of the variable is not 0 this is a further wrong attempt to discard the top of the stack. The counter keeps track of it.
+*)
+
+Definition push4 (p : Z * list Z * nat) : (Z * list Z * nat) := 
+match p with 
+| (v,   [], S n) => (v,   [],   n)
+| (v,    t,   O) => (0, v::t,   O) 
+| (0, v::t, S n) => (0, v::t, S n)
+| (k, v::t, S n) => (k, v::t,   n) 
+end.
+
+(** The definition of [push4], "complements" the one of [pop4], the "main" case being [pop4 (v,t,O) = (0,v::t,O)] which stores the current value of the variable on top of the stack, resetting it to 0, because no there is no trace of some pending wrong occurrence of [po].
+*)
+
--- SCORE/push_pop_properties.v
+++ SCORE/push_pop_properties.v
@@ -13,115 +13,6 @@ Local Open Scope list_scope.
 (** * Proof of "push/pop are each other inverse" *)
 
 (*  ******************************** *)
-(** * PUSH and POP: 4 cases version *)
-
-Lemma pu_inv_po : forall (x: Z * list Z * nat), 
-  pu (po x) = x.
-Proof. intro p. destruct p as ((v, s), c).
-destruct v as [ | vp | vn ].
-- (* v = 0 *)
-  destruct c as [ | c' ].
-  + destruct s as [ | h t ].
-    * reflexivity. 
-    * unfold po; 
-      destruct h as [ | hp | hn ];
-      destruct t as [ | h' t']; 
-      reflexivity.
-  + destruct s as [ | h t ].
-    * reflexivity. 
-    * unfold po; 
-      destruct h as [ | hp | hn ];
-      destruct t as [ | h' t']; 
-      reflexivity.
-- (* v > 0 *)
-  destruct c as [ | c' ].
-  + destruct s as [ | h t ].
-    * reflexivity. 
-    * unfold po; 
-      destruct h as [ | hp | hn ];
-      destruct t as [ | h' t']; 
-      reflexivity.
-  + destruct s as [ | h t ].
-    * reflexivity. 
-    * unfold po; 
-      destruct h as [ | hp | hn ];
-      destruct t as [ | h' t']; 
-      reflexivity.
-- (* v < 0 *)
-  destruct c as [ | c' ].
-  + destruct s as [ | h t ].
-    * simpl. reflexivity. 
-    * unfold po; 
-      destruct h as [ | hp | hn ];
-      destruct t as [ | h' t']; 
-      reflexivity.
-  + destruct s as [ | h t ].
-    * simpl. reflexivity. 
-    * unfold po; 
-      destruct h as [ | hp | hn ];
-      destruct t as [ | h' t']; 
-      reflexivity.
-Qed.
-
-
-Lemma po_inv_pu : forall (x: Z * list Z * nat), 
-  po (pu x) = x.
-Proof. intro p. destruct p as ((v, s), c).
-destruct v as [ | vp | vn ].
-- (* v = 0 *)
-  destruct c as [ | c' ].
-  + destruct s as [ | h t ].
-    * reflexivity. 
-    * unfold po; 
-      destruct h as [ | hp | hn ];
-      destruct t as [ | h' t']; 
-      reflexivity.
-  + destruct s as [ | h t ].
-    * reflexivity. 
-    * unfold po; 
-      destruct h as [ | hp | hn ];
-      destruct t as [ | h' t']; 
-      reflexivity.
-- (* v > 0 *)
-  destruct c as [ | c' ].
-  + destruct s as [ | h t ].
-    * reflexivity. 
-    * unfold po; 
-      destruct h as [ | hp | hn ];
-      destruct t as [ | h' t']; 
-      reflexivity.
-  + destruct s as [ | h t ].
-    * reflexivity. 
-    * unfold po; 
-      destruct h as [ | hp | hn ];
-      destruct t as [ | h' t']; 
-      reflexivity.
-- (* v < 0 *)
-  destruct c as [ | c' ].
-  + destruct s as [ | h t ].
-    * simpl. reflexivity. 
-    * unfold po; 
-      destruct h as [ | hp | hn ];
-      destruct t as [ | h' t']; 
-      reflexivity.
-  + destruct s as [ | h t ].
-    * simpl. reflexivity. 
-    * unfold po; 
-      destruct h as [ | hp | hn ];
-      destruct t as [ | h' t']; 
-      reflexivity.
-Qed.
-
-Theorem po_inv_pu_inv_po : forall p : Z * list Z * nat, 
-  (po (pu p) = p) /\ (pu (po p) = p).
-Proof. destruct p as ((v, s), c). intros. 
-split.
-- apply po_inv_pu.
-- apply pu_inv_po.
-Qed.
-
-
-(*  ******************************** *)
 (** * PUSH and POP: 3 cases version *)
 
 
@@ -228,4 +119,115 @@ split.
 - apply pop_inv_push.
 - apply push_inv_pop.
 Qed.
+
+(*  ******************************** *)
+(** * PUSH and POP: 4 cases version *)
+
+Lemma push4_inv_pop4 : forall (x: Z * list Z * nat), 
+  push4 (pop4 x) = x.
+Proof. intro p. destruct p as ((v, s), c).
+destruct v as [ | vp | vn ].
+- (* v = 0 *)
+  destruct c as [ | c' ].
+  + destruct s as [ | h t ].
+    * reflexivity. 
+    * unfold pop4; 
+      destruct h as [ | hp | hn ];
+      destruct t as [ | h' t']; 
+      reflexivity.
+  + destruct s as [ | h t ].
+    * reflexivity. 
+    * unfold pop4; 
+      destruct h as [ | hp | hn ];
+      destruct t as [ | h' t']; 
+      reflexivity.
+- (* v > 0 *)
+  destruct c as [ | c' ].
+  + destruct s as [ | h t ].
+    * reflexivity. 
+    * unfold pop4; 
+      destruct h as [ | hp | hn ];
+      destruct t as [ | h' t']; 
+      reflexivity.
+  + destruct s as [ | h t ].
+    * reflexivity. 
+    * unfold pop4; 
+      destruct h as [ | hp | hn ];
+      destruct t as [ | h' t']; 
+      reflexivity.
+- (* v < 0 *)
+  destruct c as [ | c' ].
+  + destruct s as [ | h t ].
+    * simpl. reflexivity. 
+    * unfold pop4; 
+      destruct h as [ | hp | hn ];
+      destruct t as [ | h' t']; 
+      reflexivity.
+  + destruct s as [ | h t ].
+    * simpl. reflexivity. 
+    * unfold pop4; 
+      destruct h as [ | hp | hn ];
+      destruct t as [ | h' t']; 
+      reflexivity.
+Qed.
+
+
+Lemma pop4_inv_push4 : forall (x: Z * list Z * nat), 
+  pop4 (push4 x) = x.
+Proof. intro p. destruct p as ((v, s), c).
+destruct v as [ | vp | vn ].
+- (* v = 0 *)
+  destruct c as [ | c' ].
+  + destruct s as [ | h t ].
+    * reflexivity. 
+    * unfold pop4; 
+      destruct h as [ | hp | hn ];
+      destruct t as [ | h' t']; 
+      reflexivity.
+  + destruct s as [ | h t ].
+    * reflexivity. 
+    * unfold pop4; 
+      destruct h as [ | hp | hn ];
+      destruct t as [ | h' t']; 
+      reflexivity.
+- (* v > 0 *)
+  destruct c as [ | c' ].
+  + destruct s as [ | h t ].
+    * reflexivity. 
+    * unfold pop4; 
+      destruct h as [ | hp | hn ];
+      destruct t as [ | h' t']; 
+      reflexivity.
+  + destruct s as [ | h t ].
+    * reflexivity. 
+    * unfold pop4; 
+      destruct h as [ | hp | hn ];
+      destruct t as [ | h' t']; 
+      reflexivity.
+- (* v < 0 *)
+  destruct c as [ | c' ].
+  + destruct s as [ | h t ].
+    * simpl. reflexivity. 
+    * unfold pop4; 
+      destruct h as [ | hp | hn ];
+      destruct t as [ | h' t']; 
+      reflexivity.
+  + destruct s as [ | h t ].
+    * simpl. reflexivity. 
+    * unfold pop4; 
+      destruct h as [ | hp | hn ];
+      destruct t as [ | h' t']; 
+      reflexivity.
+Qed.
+
+Theorem pop4_inv_push4_inv_pop4 : forall p : Z * list Z * nat, 
+  (pop4 (push4 p) = p) /\ (push4 (pop4 p) = p).
+Proof. destruct p as ((v, s), c). intros. 
+split.
+- apply pop4_inv_push4.
+- apply push4_inv_pop4.
+Qed.
+
+
+
 
